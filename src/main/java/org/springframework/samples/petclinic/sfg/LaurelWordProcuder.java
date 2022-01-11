d3c9f3d77519f06ae74b2aff0f2957967e27ede4
Robin Ghyselinck <robin.ghyselinck2@kbc.be>
2022-01-11 09:59:35 +0100

    fixed minor issue

--- src/main/java/org/springframework/samples/petclinic/sfg/LaurelWordProcuder.java
+++ src/main/java/org/springframework/samples/petclinic/sfg/LaurelWordProcuder.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
-@Profile("laurel")
 @Component
 public class LaurelWordProcuder implements WordProducer {
     @Override
